14485d388e7928fbbc154000866eb0d2879d107f
KuruKuruHerta <148481626+KuruKuruHerta@users.noreply.github.com>
2024-03-01 10:54:23 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+//Lets the selected audio play//
+let audio = document.getElementById("Wonderhoy");
+let button = document.querySelector(".Click-Button")
+function play(){
+    let audio = document.getElementById("Wonderhoy");
+    audio.play()
+}
+
+button.addEventListener("click", play)
+
+let audio2 = document.getElementById("Yatta");
+let button2 = document.querySelector(".Click-Button-2")
+function play2(){
+    let audio2 = document.getElementById("Yatta");
+    audio2.play()
+}
+
+button2.addEventListener("click", play2)
+
+let audio3 = document.getElementById("Bronya-Onee-Chan");
+let button3 = document.querySelector(".Click-Button-3")
+function play3(){
+    let audio3 = document.getElementById("Bronya-Onee-Chan");
+    audio3.play()
+}
+
+button3.addEventListener("click", play3)
